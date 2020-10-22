f1debc74c3d4fa5125a3851ab12c1e96f48fd97d
Patel210 <pranavpatel2101997@gmail.com>
2020-10-22 12:26:29 +0530

    Added test case to validate the number of entries in the most runs file

---
+++ src/test/java/com/capgemini/iplanalysertest/IPLAnalyserTest.java
@@ -0,0 +1,33 @@
+package com.capgemini.iplanalysertest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.capgemini.iplanalyser.IPLAnalyser;
+import com.capgemini.opencsvbuilder.CSVException;
+
+public class IPLAnalyserTest {
+	private static final String IPL_BATSMAN_FILE_PATH = "C:\\Users\\Pranav\\eclipse-workspace\\IPL Analyser\\src\\test\\resources\\WP DP Data_01 IPL2019FactsheetMostRuns - WP DP Data_01 IPL2019FactsheetMostRuns.csv";
+
+	private IPLAnalyser iplAnalyser;
+	
+	@Before
+	public void setup() {
+		iplAnalyser = new IPLAnalyser();
+		try {
+			iplAnalyser.loadBatsmanData(IPL_BATSMAN_FILE_PATH);
+		} catch (CSVException e) {
+		}
+	}
+	
+	@Test
+	public void givenAFileWithBatsmanData_WhenLoaded_ShouldReturnTheEntries() {
+		iplAnalyser = new IPLAnalyser();
+		try {
+			int entries = iplAnalyser.loadBatsmanData(IPL_BATSMAN_FILE_PATH);
+			assertEquals(100, entries);
+		} catch (CSVException e) {}
+	}
+}
