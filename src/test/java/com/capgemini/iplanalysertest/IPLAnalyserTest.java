58afcf31fc013e4acda846b7716838b1f3504802
Patel210 <pranavpatel2101997@gmail.com>
2020-10-22 12:34:42 +0530

    Added test case to validate the number of entries in the most wickets file

--- src/test/java/com/capgemini/iplanalysertest/IPLAnalyserTest.java
+++ src/test/java/com/capgemini/iplanalysertest/IPLAnalyserTest.java
@@ -10,6 +10,7 @@ import com.capgemini.opencsvbuilder.CSVException;
 
 public class IPLAnalyserTest {
 	private static final String IPL_BATSMAN_FILE_PATH = "C:\\Users\\Pranav\\eclipse-workspace\\IPL Analyser\\src\\test\\resources\\WP DP Data_01 IPL2019FactsheetMostRuns - WP DP Data_01 IPL2019FactsheetMostRuns.csv";
+	private static final String IPL_BOWLER_FILE_PATH = "C:\\Users\\Pranav\\eclipse-workspace\\IPL Analyser\\src\\test\\resources\\WP DP Data_02 IPL2019FactsheetMostWkts - WP DP Data_02 IPL2019FactsheetMostWkts.csv";
 
 	private IPLAnalyser iplAnalyser;
 	
@@ -18,6 +19,7 @@ public class IPLAnalyserTest {
 		iplAnalyser = new IPLAnalyser();
 		try {
 			iplAnalyser.loadBatsmanData(IPL_BATSMAN_FILE_PATH);
+			iplAnalyser.loadBowlerData(IPL_BOWLER_FILE_PATH);
 		} catch (CSVException e) {
 		}
 	}
@@ -28,6 +30,15 @@ public class IPLAnalyserTest {
 		try {
 			int entries = iplAnalyser.loadBatsmanData(IPL_BATSMAN_FILE_PATH);
 			assertEquals(100, entries);
+		} catch (CSVException e) {}
+	}
+	
+	@Test
+	public void givenAFileWithBowlerData_WhenLoaded_ShouldReturnTheEntries() {
+		iplAnalyser = new IPLAnalyser();
+		try {
+			int entries = iplAnalyser.loadBowlerData(IPL_BOWLER_FILE_PATH);
+			assertEquals(99, entries);
 		} catch (CSVException e) {}
 	}
 }
